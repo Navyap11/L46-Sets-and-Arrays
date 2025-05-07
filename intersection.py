186181d7d0f2e2b4961b227141f0a494c32048de
navyap11 <ashwin.p.parmar@gmail.com>
2025-05-07 16:12:13 +0200

    d

---
+++ intersection.py
@@ -0,0 +1,6 @@
+set1={"Green","Blue"}
+set2={"Blue", "Yellow"}
+print("The OG sets are: ",set1,"and: ",set2)
+
+set3= set1.intersection(set2)
+print("The common word(s) between the two sets are: ",set3)
