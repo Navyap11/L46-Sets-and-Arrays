1a0918a13296d803460595f91378747db11ced6d
navyap11 <ashwin.p.parmar@gmail.com>
2025-05-07 16:02:27 +0200

    d

---
+++ operations on sets.py
@@ -0,0 +1,16 @@
+set_my= {2,4,6}
+print(set_my)
+
+set_my= {0.1, "icecream", 4}
+print(set_my)
+
+set_my= {1,2,3,4,3,2,4}
+print(set_my)
+
+set_my= set([1,2,3,2])
+print(set_my)
+
+new_set= {0,1,3,4,5}
+print(new_set)
+ste1= new_set.pop()
+print(ste1)
